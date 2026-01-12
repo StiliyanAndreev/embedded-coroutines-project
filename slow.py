8e2d4ef3c5cf4ed595d87edadfa52eda364b28e7
Stiliyan Andreev <122437066+StiliyanAndreev@users.noreply.github.com>
2026-01-12 10:23:54 +0100

    Implement multiple socket connections in slow.py
    Added a second socket connection to simulate multiple requests.

--- slow.py
+++ slow.py
@@ -6,10 +6,23 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('localhost', 8080))
 
 print("--- Client A Connected! I will sleep for 10 seconds... ---")
-# This simulates a slow connection.
 time.sleep(10)
 
 print("--- Client A Waking up and sending data! ---")
-s.send(b"GET / HTTP/1.0\r\n\r\n")
-print("Response:", s.recv(1024).decode())
+s.send(b"GET / HTTP/1.0")
+
+
+s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s2.connect(('localhost', 8080))
+
+print("--- Client A Connected! I will sleep for 10 seconds... ---")
+time.sleep(10)
+
+print("--- Client A Waking up and sending data! ---")
+s2.send(b"GET / HTTP/1.0\r\n\r\n")
+print("Response:", s2.recv(1024).decode())
+
+
+
+
 s.close()
